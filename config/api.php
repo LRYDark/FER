ef9b171ac2c76c4dbd1cdfbacde7f2e66556f9de
LRYDark <reinert.joris@gmail.com>
2026-04-24 11:10:21 +0200

    1.0.5

--- config/api.php
+++ config/api.php
@@ -78,7 +78,13 @@ function isIpBanned($pdo, $ip) {
     return getIpBanInfo($pdo, $ip) !== null;
 }
 
-function getClientIp() {
+function getClientIp(): string {
+    if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+        return $_SERVER['HTTP_CF_CONNECTING_IP'];
+    }
+    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        return trim(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])[0]);
+    }
     return $_SERVER['REMOTE_ADDR'] ?? '0.0.0.0';
 }
 
